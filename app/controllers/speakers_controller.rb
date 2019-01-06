62f62a5f03e260ffd2e9ec948b32dae9cde782ee
„Madeleine <madeleine.neumann@9elements.com>
2019-01-06 02:23:04 +0100

    adds further speaker details

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -1,2 +1,9 @@
 class SpeakersController < ApplicationController
+  def index
+    @speakers = Speaker.all
+  end
+
+  def show
+    @speaker = Speaker.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :companies, only: [:index, :show]
+  resources :speakers, only: [:index, :show]
   resources :todos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
